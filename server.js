e1878a2abdf6d2caa97031d8a26214b06c4f6261
laushinka <laurie.malau@gmail.com>
2016-09-09 21:45:59 -0400

    No change

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
-var app = express();
-var port = 3000;
+var app     = express();
+var port    = 3000;
 
 app.listen(port, function(err){
   console.log('Running fine on port ' + port);
