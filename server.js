47f672681812a78bba365ddb01fbf72a7fcd784a
laushinka <laurie.malau@gmail.com>
2016-09-09 21:47:32 -0400

    Add get request action

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ var express = require('express');
 var app     = express();
 var port    = 3000;
 
+app.get('/', function(req, res){
+  res.render(__dirname + '/views/index');
+})
+
 app.listen(port, function(err){
   console.log('Running fine on port ' + port);
 })
