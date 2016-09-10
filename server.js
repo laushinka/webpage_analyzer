55db845b87e2405e5e9f67e56e9b7b8e37ea7bf7
laushinka <laurie.malau@gmail.com>
2016-09-09 22:02:58 -0400

    Specify path for get request

--- server.js
+++ server.js
@@ -2,12 +2,13 @@ var express = require('express');
 var app     = express();
 var port    = 3000;
 
-app.get('/', function(req, res){
-  res.render(__dirname + '/views/index');
-})
-
 // Express requires a view engine
 app.set('view engine', 'ejs');
+
+app.get('/', function(req, res){
+  console.log(__dirname);
+  res.render(__dirname + '/views/index');
+})
 
 app.listen(port, function(err){
   console.log('Running fine on port ' + port);
