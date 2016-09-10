6bd5c3c9420434df1fba182e52d13c6ee9273adb
laushinka <laurie.malau@gmail.com>
2016-09-10 00:47:42 -0400

    Install cheerio

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express     = require('express');
 var app         = express();
 var bodyParser  = require('body-parser');
 var request     = require('request');
+var cheerio     = require('cheerio');
 var port        = 2016;
 
 var data = {
