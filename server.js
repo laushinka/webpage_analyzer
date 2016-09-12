0d67cff60ee4d6f44627a0563ae4a8d0aed77dae
laushinka <laurie.malau@gmail.com>
2016-09-11 20:59:32 -0400

    Still doesn't work

--- server.js
+++ server.js
@@ -44,117 +44,117 @@ app.post('/', function(req, res){
     if (error) {
       // data.status_code = error.statusCode;
       res.render(__dirname + '/views/index', {error: true, error_message: error});
+    }
+    if (response.statusCode !== 200) {
+        res.render(__dirname + '/views/index', {error: true,error_message: 'status code is ' + response.statusCode});
+        return;
+      }
+
+    // HTML version
+    var html = body.toLowerCase();
+    var doctypes = html.match(/<!doctype html(.*?)>/);
+    // console.log(doctypes);
+    if (doctypes && doctypes[0] == '<!doctype html>') {
+      data.doctype = 'HTML 5.0';
+    } else if (doctypes[0].search('html 4.01')) {
+      data.doctype = 'HTML 4.01';
+    } else if (doctypes[0].search('xhtml 1.1')) {
+      data.doctype = 'XHTML 1.1';
+    } else if (doctypes[0].search('xhtml 1.0')) {
+      data.doctype = 'XHTML 1.0';
     } else {
-      if (response.statusCode !== 200) {
-        res.render(__dirname + '/views/index', {error: true, error_message: 'Status code is ' + response.statusCode})
+      data.doctype = 'No HTML version found';
     }
 
-      // HTML version
-      var html = body.toLowerCase();
-      var doctypes = html.match(/<!doctype html(.*?)>/);
-      // console.log(doctypes);
-      if (doctypes && doctypes[0] == '<!doctype html>') {
-        data.doctype = 'HTML 5.0';
-      } else if (doctypes[0].search('html 4.01')) {
-        data.doctype = 'HTML 4.01';
-      } else if (doctypes[0].search('xhtml 1.1')) {
-        data.doctype = 'XHTML 1.1';
-      } else if (doctypes[0].search('xhtml 1.0')) {
-        data.doctype = 'XHTML 1.0';
-      } else {
-        data.doctype = 'No HTML version found';
+    var $ = cheerio.load(html);
+
+    // Title
+    data.title = _.upperFirst($('title').text());
+
+    // Page headings
+    data.h1 = $('h1').length;
+    data.h2 = $('h2').length;
+    data.h3 = $('h3').length;
+    data.h4 = $('h4').length;
+    data.h5 = $('h5').length;
+    data.h6 = $('h6').length;
+
+    // Number of links
+    var links = $('a');
+    data.links_count = links.length;
+    var outstanding_requests = links.length;
+
+    // External links
+    function isExternal(url_input){
+      var href_hostname = url.parse(url_input.attribs.href).hostname;
+      console.log(href_hostname, 'Original hostname')
+      if (href_hostname !== null) {
+        href_hostname = href_hostname.split('.');
+        var href_length = href_hostname.length
+        if (href_length > 2) {
+          // 012345
+          href_hostname = href_hostname[href_length-2] + '.' +href_hostname[href_length-1]
+        } else {
+          href_hostname = href_hostname.join('.');
+        }
+      }
+      var user_hostname = url.parse(req.body.url).hostname;
+      if (user_hostname !== null) {
+        user_hostname = user_hostname.split('.');
+        var user_length = user_hostname.length
+        if (user_length > 2) {
+            user_hostname = user_hostname[user_length-2] + '.' +user_hostname[user_length-1]
+        } else {
+          user_hostname = user_hostname.join('.')
+        }
       }
 
-      var $ = cheerio.load(html);
+      console.log(href_hostname)
+      console.log(user_hostname)
+      console.log(url_input.attribs.href)
+      console.log(req.body.url)
+      console.log(href_hostname != null, 'False means it is null')
+      console.log(href_hostname !== user_hostname, 'False means it is an internal link')
 
-      // Title
-      data.title = _.upperFirst($('title').text());
+      return href_hostname !== null && href_hostname !== user_hostname;
+    }
 
-      // Page headings
-      data.h1 = $('h1').length;
-      data.h2 = $('h2').length;
-      data.h3 = $('h3').length;
-      data.h4 = $('h4').length;
-      data.h5 = $('h5').length;
-      data.h6 = $('h6').length;
+    // Number of external links
+    data.external_links = _.filter($('a'), isExternal).length;
 
-      // Number of links
-      var links = $('a');
-      data.links_count = links.length;
-      var outstanding_requests = links.length;
+    // Number of internal links
+    data.internal_links = _.filter($('a'), function(link) { return !isExternal(link) }).length
 
-      // External links
-      function isExternal(url_input){
-        var href_hostname = url.parse(url_input.attribs.href).hostname;
-        console.log(href_hostname, 'Original hostname')
-        if (href_hostname !== null) {
-          href_hostname = href_hostname.split('.');
-          var href_length = href_hostname.length
-          if (href_length > 2) {
-            // 012345
-            href_hostname = href_hostname[href_length-2] + '.' +href_hostname[href_length-1]
-          } else {
-            href_hostname = href_hostname.join('.');
-          }
-        }
-        var user_hostname = url.parse(req.body.url).hostname;
-        if (user_hostname !== null) {
-          user_hostname = user_hostname.split('.');
-          var user_length = user_hostname.length
-          if (user_length > 2) {
-              user_hostname = user_hostname[user_length-2] + '.' +user_hostname[user_length-1]
-          } else {
-            user_hostname = user_hostname.join('.')
-          }
-        }
-
-        console.log(href_hostname)
-        console.log(user_hostname)
-        console.log(url_input.attribs.href)
-        console.log(req.body.url)
-        console.log(href_hostname != null, 'False means it is null')
-        console.log(href_hostname !== user_hostname, 'False means it is an internal link')
-
-        return href_hostname !== null && href_hostname !== user_hostname;
+    // Number of broken links
+    _.each(links, function(link){
+      var url_link = link.attribs.href;
+      if(url_link.indexOf('http') === -1){
+          url_link = 'http:' + url_link;
       }
-
-      // Number of external links
-      data.external_links = _.filter($('a'), isExternal).length;
-
-      // Number of internal links
-      data.internal_links = _.filter($('a'), function(link) { return !isExternal(link) }).length
-
-      // Number of broken links
-      _.each(links, function(link){
-        var url_link = link.attribs.href;
-        if(url_link.indexOf('http') === -1){
-            url_link = 'http:' + url_link;
+      request(url_link, function(error, response, body){
+        // console.log(response)
+        if(error){
+          console.log(error, 'Error');
+        }else{
+          if(response.statusCode !== 200){
+            data.broken_links++;
+          }
         }
-        request(url_link, function(error, response, body){
-          // console.log(response)
-          if(error){
-            console.log(error, 'Error');
-          }else{
-            if(response.statusCode !== 200){
-              data.broken_links++;
-            }
-          }
-          outstanding_requests--;
-          if (outstanding_requests === 0) {
-            res.render(__dirname + '/views/index', { error: false, data: data });
-            console.log(outstanding_requests);
-            console.log('This is the outstanding requests function')
-          }
-          console.log(data.broken_links);
-        //  console.log(response);
-        })
+        outstanding_requests--;
+        if (outstanding_requests === 0) {
+          res.render(__dirname + '/views/index', { error: false, data: data });
+          console.log(outstanding_requests);
+          console.log('This is the outstanding requests function')
+        }
+        console.log(data.broken_links);
+      //  console.log(response);
       })
+    })
 
-      // Is there a login/signup form
-      if ($('input[type="password"]').length > 0) {
-        console.log($('input[type="password"]'));
-        data.login_form = "Yes";
-      }
+    // Is there a login/signup form
+    if ($('input[type="password"]').length > 0) {
+      console.log($('input[type="password"]'), 'Password');
+      data.login_form = "Yes";
     }
   });
 })
