0b8c508791964d54180e6f308049998d8bab3537
laushinka <laurie.malau@gmail.com>
2016-09-15 21:20:03 -0400

    Remove adding protocol for url_link, due to different format in each site

--- server.js
+++ server.js
@@ -97,7 +97,7 @@ app.post('/', function(req, res){
         outstanding_requests--;
         return;
       };
-      var url_link = link.attribs.href.startsWith('http:') ? link.attribs.href : 'http:' + link.attribs.href;
+      var url_link = link.attribs.href
 
       request(url_link, function(error, response, body){
         if(error){
